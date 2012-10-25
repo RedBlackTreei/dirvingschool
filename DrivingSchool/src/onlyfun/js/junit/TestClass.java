639288118dee93101710c62732eacec1f43c5684
纪森 <240896322@qq.com>
2012-10-25 08:31:19 +0800

    TestClass.java
    删除一些不必要的引用

--- DrivingSchool/src/onlyfun/js/junit/TestClass.java
+++ DrivingSchool/src/onlyfun/js/junit/TestClass.java
@@ -1,14 +1,8 @@
 package onlyfun.js.junit;
 
-import java.util.Date;
-
-import onlyfun.js.model.Coach;
-import onlyfun.js.model.Person;
-import onlyfun.js.model.Student;
 import onlyfun.js.uitl.HibernateUtil;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
@@ -21,7 +15,6 @@ public class TestClass {
 	
 	Session session = HibernateUtil.getSessionFactory().openSession();
 	Transaction transaction = null;
-	@SuppressWarnings("deprecation")
 	@Test
 	public void createTable(){
 		 Configuration configuration = new AnnotationConfiguration(); 
